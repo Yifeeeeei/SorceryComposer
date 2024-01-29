90d6391fc272984977116cade2b9d5cfcb9a1d7d
if <55980773+Yifeeeeei@users.noreply.github.com>
2024-01-28 20:37:01 -0800

    test json

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,4 @@
 console.log("Hello from main.js");
+
+let card_infos = read_all_card_infos();
+console.log(card_infos.length);
---
+++ scripts/utils.js
@@ -0,0 +1,12 @@
+function hello_utils() {
+    console.log("Hello from utils.js");
+}
+
+function read_all_card_infos() {
+    let all_cards_info_json_path = "resources/all_cards_info.json";
+    var card_infos = [];
+    $.getJSON(all_cards_info_json_path, function (json) {
+        card_infos = json;
+    });
+    return card_infos;
+}
