2c94b777119b1640217a1292f6d7001a153d7795
if <55980773+Yifeeeeei@users.noreply.github.com>
2024-07-08 22:04:38 -0700

    fix place holder issue

--- Arena/scripts/arena_global_vars.js
+++ Arena/scripts/arena_global_vars.js
@@ -2,3 +2,14 @@ var arena_candidate_number = 3;
 var arena_chosen_element = "?";
 // arena_card_info_added is used to communicate bewteen main loop and after choosing a card
 var arena_card_info_added = null;
+
+var element_placeholders = {
+    光: "../resources/placeholders/ele_light.png",
+    暗: "../resources/placeholders/ele_dark.png",
+    水: "../resources/placeholders/ele_water.png",
+    火: "../resources/placeholders/ele_fire.png",
+    地: "../resources/placeholders/ele_earth.png",
+    气: "../resources/placeholders/ele_air.png",
+    无: "../resources/placeholders/ele_none.png",
+    // "?": "resources/placeholders/ele_none.png",
+};
